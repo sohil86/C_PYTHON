df8ed1c7695effc6d9fc26c777b63925ebb9b4c4
devajparikh <devajnparikh@gmail.com>
2017-09-29 15:26:18 +0530

    Adding the files to repository for the first time

---
+++ PROG_RANDOM_NUMBER/README.txt
@@ -0,0 +1,9 @@
+1. The "source" folder has the source code (RANDOM_NUMBER.c) and the object file (RANDOM_NUMBER.o).
+
+2. The "include" folder has the header file (HEADER.h).
+
+3. The "build" folder contains the Makefile.
+
+4. The "bin" folder contains the executable file.
+
+5. To run the executable on command line, go to bin folder and write ./RANDOM_NUMBER
---
+++ SHELL.sh
@@ -0,0 +1,26 @@
+cd PROG_RANDOM_NUMBER/build
+make
+cd ../..
+for number in `seq 1 100`
+do
+echo
+echo
+echo =============================================
+echo
+echo TEST \# $number
+echo .............................................
+cd PROG_RANDOM_NUMBER/bin
+./RANDOM_NUMBER
+exit_status=$?
+cd ../..
+#echo
+#echo .............................................
+echo
+echo RANDOM NUMBER: $exit_status
+echo
+python PRIME.py $exit_status
+echo
+echo ==============================================
+echo
+echo
+done
